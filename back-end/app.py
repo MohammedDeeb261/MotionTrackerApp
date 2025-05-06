331a1545edfff810b2cad77cd4ebd5afafc7cb8a
MohammedDeeb261 <mdeeb@sfsu.edu>
2025-05-06 09:25:12 -0700

    training model3

--- back-end/app.py
+++ back-end/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+from train_model import train_model
 app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
@@ -8,3 +8,9 @@ def predict():
     # run your model here (mocked below)
     prediction = "running"
     return jsonify({"prediction": prediction})
+
+
+@app.route('/train', methods=['POST'])
+def train():
+    train_model("backend/dataset")  # Points to the folder where CSVs are
+    return jsonify({"status": "Model trained and saved"})
